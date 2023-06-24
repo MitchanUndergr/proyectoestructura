f288f18f77a8abbb3985a694afa15c6e130a8574
MitchanUndergr <131836786+MitchanUndergr@users.noreply.github.com>
2023-06-23 23:49:11 -0400

    se agrega 20 repeticiones y archivo

--- countRegion20.cpp
+++ countRegion20.cpp
@@ -1,39 +1,42 @@
 #include "Quad.h"
 #include <chrono>
+#include <fstream>
+#include <iostream>
 #include <random>
+#include <sstream>
 #include <vector>
 
 int main() {
-    Point topLeft(-120.0, -120.0);
-    Point botRight(120.0, 120.0);
+    Point topLeft(-180.0, -180.0);
+    Point botRight(180.0, 180.0);
 
     Quad quadtree(topLeft, botRight);
 
-    ifstream inputFile("C:/Users/user/Downloads/ESTRUCTURA/Experimental2/datos20.txt");
-    string line;
+    std::ifstream inputFile("C:/Users/user/Downloads/ESTRUCTURA/Experimental2/datos20.txt");
+    std::string line;
 
-    vector<Node*> nodos;
+    std::vector<Node*> nodos;
 
-    while (getline(inputFile, line)) {
-        stringstream ss(line);
+    while (std::getline(inputFile, line)) {
+        std::stringstream ss(line);
 
-        string poblacionStr, latitudeStr, longitudStr;
-        getline(ss, poblacionStr, ';');    // leer como cadena
-        getline(ss, latitudeStr, ';');
-        getline(ss, longitudStr, ';');
+        std::string poblacionStr, latitudeStr, longitudStr;
+        std::getline(ss, poblacionStr, ';');    // leer como cadena
+        std::getline(ss, latitudeStr, ';');
+        std::getline(ss, longitudStr, ';');
 
-        int poblacion = stoi(poblacionStr);
+        int poblacion = std::stoi(poblacionStr);
         size_t commaPos1 = latitudeStr.find(',');
-        if (commaPos1 != string::npos) {
+        if (commaPos1 != std::string::npos) {
             latitudeStr[commaPos1] = '.';
         }
-        double latitud = stod(latitudeStr);
+        double latitud = std::stod(latitudeStr);
 
         size_t commaPos2 = longitudStr.find(',');
-        if (commaPos2 != string::npos) {
+        if (commaPos2 != std::string::npos) {
             longitudStr[commaPos2] = '.';
         }
-        double longitud = stod(longitudStr);
+        double longitud = std::stod(longitudStr);
 
         Point point(latitud, longitud);
         Node* nuevoNode = new Node(point, poblacion);
@@ -42,23 +45,37 @@ int main() {
         quadtree.insert(nuevoNode);
     }
 
-
     Point regionCenter(0, 0);
-    int regionRadius[] = {2,30,50, 100, 180};
+    int regionRadius[] = {2, 30, 50, 100, 180};
 
-    for(int j=0; j<5;j++) {
+    std::ofstream outputFile("C:/Users/user/Downloads/ESTRUCTURA/Experimental2/countRegion20.txt");
 
-        cout << "tiempo ejecucion countRegion 20 datos con centro P(0,0) y area del cuadrado "<<regionRadius[j]<< "x" <<regionRadius[j] << endl;
-        auto start1 = chrono::system_clock::now();
+    if (outputFile.is_open()) {
+        for (int j = 0; j < 5; ++j) {
+            outputFile << "Region " << regionRadius[j] << "x" << regionRadius[j] << ":" << std::endl;
+            for (int k = 0; k < 20; ++k) {
+                std::cout << "Tiempo de ejecucion countRegion 20 datos con centro P(0,0) y area del cuadrado " << regionRadius[j] << "x" << regionRadius[j] << " (iteracion " << (k + 1) << "):" << endl;
+                auto start1 = std::chrono::system_clock::now();
 
-        for (int i = 0; i < nodos.size(); i++) {
-            quadtree.countRegion(nodos, regionCenter, regionRadius[j]);
+                for (int i = 0; i < nodos.size(); ++i) {
+                    quadtree.countRegion(nodos, regionCenter, regionRadius[j]);
+                }
+
+                auto end1 = std::chrono::system_clock::now();
+                std::chrono::duration<double, std::milli> duration = end1 - start1;
+                double tiempo = duration.count();
+                cout << tiempo << "ms" << endl;
+
+                outputFile << tiempo << endl;
+            }
+            outputFile << endl;
         }
-        auto end1 = chrono::system_clock::now();
-        chrono::duration<float, milli> duration = end1 - start1;
-        cout << duration.count() << "ms" << endl;
-
+        outputFile.close();
+        cout << "Los tiempos se han guardado en el archivo countRegion20.txt" << endl;
+    } else {
+        cout << "No se pudo abrir el archivo countRegion20.txt" << endl;
     }
+
     for (const auto& nodo : nodos) {
         delete nodo;
     }
